9bd0957cf19204f8c437380f80da0a86776cff00
Keith Jerosky <kjerosky@gmail.com>
2025-04-17 16:23:24 -0700

    Add normal maps to tree billboard and scale tree model down

--- scripts/botw_lod_tree.gd
+++ scripts/botw_lod_tree.gd
@@ -4,7 +4,7 @@ extends Node3D
 @onready var model := $Model
 @onready var billboard := $Billboard
 
-const BILLBOARD_ACTIVIATION_DISTANCE := 50.0
+const BILLBOARD_ACTIVIATION_DISTANCE := 100.0
 
 # ---------------------------------------------------------------------------
 
@@ -14,17 +14,32 @@ func _ready():
 
 # ---------------------------------------------------------------------------
 
-func _process(delta):
-	var distance_to_camera := calculate_distance_to_camera()
-	if distance_to_camera >= BILLBOARD_ACTIVIATION_DISTANCE:
+func _process(_delta):
+	var lateral_distance_to_camera := calculate_lateral_distance_to_camera()
+	if lateral_distance_to_camera >= BILLBOARD_ACTIVIATION_DISTANCE:
 		model.visible = false
 		billboard.visible = true
 	else:
 		model.visible = true
 		billboard.visible = false
+	
+	# Uncomment this section to enable manual lod toggling (and comment the above too!)
+	#if Input.is_action_just_pressed("debug_toggle_lod"):
+		#print(calculate_distance_to_camera())
+		#if model.visible:
+			#model.visible = false
+			#billboard.visible = true
+		#else:
+			#model.visible = true
+			#billboard.visible = false
 
 # ---------------------------------------------------------------------------
 
-func calculate_distance_to_camera() -> float:
+func calculate_lateral_distance_to_camera() -> float:
+	var my_position := global_position
+	my_position.y = 0.0
+	
 	var camera_position := get_viewport().get_camera_3d().global_position
+	camera_position.y = 0.0
+	
 	return self.global_position.distance_to(camera_position)
