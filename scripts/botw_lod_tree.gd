cc4c09e83f79e55e309fee614b6cd4f91d8575ec
Keith Jerosky <kjerosky@gmail.com>
2025-04-17 18:45:24 -0700

    Match tree billboard image to its player-viewed rotation

--- scripts/botw_lod_tree.gd
+++ scripts/botw_lod_tree.gd
@@ -1,10 +1,17 @@
 class_name BotwLodTree
 extends Node3D
 
+@export var player : Player
+@export var diffuse_textures : Array[Texture2D]
+@export var normal_textures : Array[Texture2D]
+
 @onready var model := $Model
 @onready var billboard := $Billboard
+@onready var billboard_material := billboard.get_surface_override_material(0) as StandardMaterial3D
 
 const BILLBOARD_ACTIVIATION_DISTANCE := 100.0
+
+var billboard_image_index := 0
 
 # ---------------------------------------------------------------------------
 
@@ -15,6 +22,20 @@ func _ready():
 # ---------------------------------------------------------------------------
 
 func _process(_delta):
+	var angle_from_player_to_tree := rad_to_deg((-self.basis.z).signed_angle_to(self.global_position - player.global_position, Vector3.DOWN))
+	if angle_from_player_to_tree < 0.0:
+		angle_from_player_to_tree = 360.0 + angle_from_player_to_tree
+	
+	# Adjust the angle so a 45 degree field is created around each major angle increment (0, 45, 90, 135, etc.)
+	var adjusted_angle := roundi(angle_from_player_to_tree + 45 / 2.0)
+	if adjusted_angle >= 360:
+		adjusted_angle -= 360
+	
+	var new_billboard_image_index := adjusted_angle / 45
+	if new_billboard_image_index != billboard_image_index:
+		billboard_image_index = new_billboard_image_index
+		update_billboard_material()
+	
 	var lateral_distance_to_camera := calculate_lateral_distance_to_camera()
 	if lateral_distance_to_camera >= BILLBOARD_ACTIVIATION_DISTANCE:
 		model.visible = false
@@ -25,7 +46,7 @@ func _process(_delta):
 	
 	# Uncomment this section to enable manual lod toggling (and comment the above too!)
 	#if Input.is_action_just_pressed("debug_toggle_lod"):
-		#print(calculate_distance_to_camera())
+		#print(calculate_lateral_distance_to_camera())
 		#if model.visible:
 			#model.visible = false
 			#billboard.visible = true
@@ -43,3 +64,9 @@ func calculate_lateral_distance_to_camera() -> float:
 	camera_position.y = 0.0
 	
 	return self.global_position.distance_to(camera_position)
+
+# ---------------------------------------------------------------------------
+
+func update_billboard_material() -> void:
+	billboard_material.albedo_texture = diffuse_textures[billboard_image_index]
+	billboard_material.normal_texture = normal_textures[billboard_image_index]
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,23 @@
 class_name Player
 extends CharacterBody3D
 
+@onready var camera = $Camera
+
 const WALK_SPEED = 5.0
 const RUN_SPEED = 15.0
-const JUMP_VELOCITY = 4.5
+
+var mouse_motion := Vector2.ZERO
+
+# ---------------------------------------------------------------------------
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+# ---------------------------------------------------------------------------
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		mouse_motion = event.relative
 
 # ---------------------------------------------------------------------------
 
@@ -11,24 +25,33 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("debug_quit"):
 		get_tree().quit()
 	
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	handle_mouse_motion(delta)
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	var speed = RUN_SPEED if Input.is_action_pressed("run") else WALK_SPEED
+	var movement_speed = RUN_SPEED if Input.is_action_pressed("run") else WALK_SPEED
 	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
+		velocity.x = direction.x * movement_speed
+		velocity.z = direction.z * movement_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, movement_speed)
+		velocity.z = move_toward(velocity.z, 0, movement_speed)
 	
 	move_and_slide()
+
+# ---------------------------------------------------------------------------
+
+func handle_mouse_motion(delta: float) -> void:
+	if mouse_motion == Vector2.ZERO:
+		return
+	
+	rotate_y(-mouse_motion.x * delta)
+	rotation_degrees.y = wrap(rotation_degrees.y, 0.0, 360.0)
+	
+	camera.rotate_x(mouse_motion.y * delta)
+	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -89.0, 89.0)
+	
+	mouse_motion = Vector2.ZERO
